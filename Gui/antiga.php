c39daf8c44c53c489524c632e0430eed921f7df3
Guilherme Soares <guisenai.oliveira11@gmail.com>
2024-11-21 15:41:35 -0300

    colocando o css

---
+++ Gui/antiga.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>Site ANTIGO</h1>
+        <div class="proximo">
+            <a href="#">Proximo</a>
+
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <p>Essa foi a página Inicial, a qual apresentei na primeira Feira de Tecnologia:</p>
+            <div><img src="paginafrente.png" alt=""></div>
+            <p>Coloquei em prática tudo o que aprendi, desenvolvendo uma página para apresentar a profissão
+                caminhoneiro, divindo essa em outras, separadas em profissão, marcas famosas, empresas envolvidas...  </p>
+                <div><img src="empresas.png" alt=""></div>
+                <div><img src="outraparte.png" alt=""></div>
+                <p>No fim uma pagina para contatos onde esta localizada o feedback.</p>
+                <div><img src="footer.png" alt=""></div>
+                
+        </div>
+        <a href="#">Proximo</a>
+    </main>
+
+
+
+
+
+
+</body>
+
+</html>
--- Gui/index.php
+++ Gui/index.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <header>
+        <div class="logo"><img src="eufotobola.jpg" alt=""><h1>Evolução SENAI</h1></div>
+        
+
+    </header>
+
+
+    <main>
+        <div class="menu">
+            <div class="link"><a href="index.php">Inicio</a></div>
+            <div class="link"><a href="antiga.php">Página Antiga</a></div>
+            <div class="link"><a href="atual.php">Página Atual</a></div>
+            <div class="link"><a href="conclusao.php">Conclusão</a></div>
+        </div>
+    </main>
+    <footer>
+        <div>
+            <p>Guilherme Soares de Oliveira</p>
+            <p> © Todos os direitos reservados</p>
+        </div>
+
+    </footer>
 </body>
+
 </html>
