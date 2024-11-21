103e7941e4e9232789d06e63e78bb1c9d4f10fca
Roberto H Silva <robertosilva.sesisenai@gmail.comgit config --global user.namegit config --global user.email robertosilva.sesisenai@gmail.com>
2024-11-21 10:16:32 -0300

    Criação dos Links

--- Evolucao.php
+++ Evolucao.php
@@ -1,20 +1,29 @@
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Evolução
-        
-    </title>
+    <title>Evolução</title>
+    <style>
+
+    </style>
 </head>
 <body>
+
+<div class="titulo">
+    <h1>Trabalhos</h1>
+</div>
+
+<div class="trabalhos">
+
+<a href="Roberto/oi.php">Trabalho do Roberto</a>
+<a href="">Trabalho da Sara</a>
+<a href="">Trabalho do Guilherme</a>
+<a href="">Trabalho do André</a>
+<a href="">Trabalho do Octavio</a>
+<a href="">Trabalho da Kauany</a>
+
+</div>
     
 </body>
 </html>
