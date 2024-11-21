f26e6eb71957641bbb54a456d2398282fc98ee54
Sara Ajala <sara.ajalasiva24@gmail.com>
2024-11-21 15:37:56 -0300

    inicio da página primeiroprojeto.php

---
+++ Sara/primeiroprojeto.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>EVOLUÇÃO</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h2 class="titulo"><a class="titula" href="evolucao.php">Evolução</a></h2>
+
+        <ul class="menu">
+            <li><a class="link" href="primeiroprojeto.php">Primeiro Projeto</a></li>
+            <li><a class="link" href="projetoatual.php">Projeto Atual</a></li>
+            <li><a class="link" href="dificuldades.php">Dificuldades</a></li>
+            <li><a class="link" href="evolucaopessoal.php">Evolução Pessoal</a></li>
+            <li><a class="link" href="contatos.php">Contatos</a></li>
+        </ul>
+    </header>
+
+    <section class="inform4">
+        <img class="imgprojeto1" src="img/fundo.png" alt="fundo projeto 1">
+        <p>Este projeto foi desenvolvido no primeiro semestre do curso, onde apresentamos um ensaio sobre diferentes profissões, <br> algumas categorias representadas foram (Campos de atuação, materiais utilizados, empresas envolvidas, entre outros tópicos).</p>
+    </section>
+
+    <section class="inform5">
+        <h3 class="subtitulo3">Primeiras Informações</h3>
+
+        <div class="content-wrapper">
+            <img class="imgprofissoes" src="img/Captura de tela 2024-11-21 151238.png" alt="profissões">
+            <p>Com a influência dos professores, construímos a partir de campos e parâmetros diferentes funções durante essa tarefa, como por exemplo, listas e carrosséis de imagens de forma simples e dinâmica.</p>
+            <img class="imgprofissoes" src="img/carrossel.png" alt="carrossel">
+        </div>
+    </section>
+
+    <footer class="rodape2">
+        <div>
+            <p>Termos e condições de uso | Política de Privacidade <br> © Sara Ajala. Todos os direitos reservados.</p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
