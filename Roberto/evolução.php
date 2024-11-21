dc9d389015ff2d31c262d197ebb6345441e0d33a
Roberto H Silva <robertosilva.sesisenai@gmail.comgit config --global user.namegit config --global user.email robertosilva.sesisenai@gmail.com>
2024-11-21 15:09:05 -0300

    Teste123

--- Roberto/evolução.php
+++ Roberto/evolução.php
@@ -1,16 +1,60 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Evolução Roberto</title>
     <style>
-
+.container{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    margin: 0 auto;
+    background-color: #caf0f8;
+    width: 300px;
+    height: 350px;
+    gap: 20px;
+    box-shadow: 2px 2px 2px  black;
+    border-radius: 10px;
+}
+a{
+    border-radius: 10px;
+    box-shadow: 2px 2px 2px #023e8a;
+    text-decoration: none;
+    color: #023047;
+    background-color: #219ebc;
+    width: 150px;
+    height:30px;
+    text-align: center;
+    font-size:15px;
+}
+h1{
+    color: #023047;
+    margin-top: -10px;
+    
+}
+body{
+    margin-top: 200px;
+    background-color: #669bbc;
+}
     </style>
 </head>
+
 <body>
 
-    
+
+    <div class="container">
+        <h1>
+            Minha Evolução
+        </h1>
+        <a href="primeiro_projeto.html">Meu primeiro projeto</a>
+        <a href="#">Meu projeto atual</a>
+        <a href="#">Dificuldades</a>
+        <a href="#">Evolução Pessoal</a>
+    </div>
 
 </body>
+
 </html>
