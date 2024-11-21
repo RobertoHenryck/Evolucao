fa519b4ea5dc86b33490a38ffea1b3e6c4476925
Andre Oliveira <lelis.andreoliveira@hotmail.com>
2024-11-21 10:35:38 -0300

    andre

---
+++ andre/evolucao.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro - Spotify</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-box">
+            <a href="login.html"><h1 class="Evoluçao">Evoluçao</h1></a>
+            
+            <div class="">
+            <button class="Evolucao">
+                
+                Primeiro Projeto
+            </button>
+            <br><br>
+            <button class="Evolucao">
+                
+                Projeto Atual
+            </button>
+            <br><br>
+            <button class="Evolucao">
+                
+                Dificuldades Encontradas
+            </button>
+            <br><br>
+            <button class="Evolucao">
+                Evolucao Pessoal
+            </button>
+        </div>
