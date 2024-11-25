c938297acfb04c165193b022b08f114a43d99652
Roberto H Silva <robertosilva.sesisenai@gmail.comgit config --global user.namegit config --global user.email robertosilva.sesisenai@gmail.com>
2024-11-25 13:39:05 -0300

    Término do meu site

--- Roberto/evolução.php
+++ Roberto/evolução.php
@@ -6,49 +6,65 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Evolução Roberto</title>
     <style>
-.container{
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    margin: 0 auto;
-    background-color: #caf0f8;
-    width: 300px;
-    height: 350px;
-    gap: 15px;
-    box-shadow: 2px 2px 2px  black;
-    border-radius: 10px;
-    
-}
-a{
-    display: flex;
-    border-radius: 10px;
-    box-shadow: 2px 2px 2px #023e8a;
-    text-decoration: none;
-    color: #023047;
-    background-color: #8ecae6;
-    width: 150px;
-    height:40px;
-    text-align: center;
-    font-size:15px;
-    justify-content: center;
-    align-items: center;
-    
+        .container {
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+            margin: 0 auto;
+            background-color: #dee2e6;
+            width: 300px;
+            height: 350px;
+            gap: 15px;
+            box-shadow: 2px 2px 2px black;
+            border-radius: 10px;
 
-    
-}
-h1{
-    color: #023047;
-    margin-top: -10px;
-    text-align: center;
-    
-}
-body{
-    margin-top: 200px;
-    background-color: #8ecae6   ;
-    align-items: center;
-    text-align: center;
-}
+        }
+
+        footer {
+            height: 100px;
+            background-color: #bcb8b1;
+            display: flex;
+            text-align: center;
+
+
+        }
+
+        a {
+            display: flex;
+            border-radius: 10px;
+            box-shadow: 2px 2px 2px #b1a7a6;
+            text-decoration: none;
+            color: black;
+            background-color: #e9ecef;
+            width: 150px;
+            height: 40px;
+            text-align: center;
+            font-size: 15px;
+            justify-content: center;
+            align-items: center;
+
+
+
+        }
+
+        h1 {
+            color: black;
+            margin-top: -10px;
+            text-align: center;
+
+        }
+
+        body {
+            margin-top: 200px;
+            background-color: #f5f3f4;
+            align-items: center;
+            text-align: center;
+            font-family: "Parkinsans", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 300;
+            font-style: normal;
+        }
     </style>
 </head>
 
@@ -62,7 +78,7 @@ body{
         <a href="primeiro_projeto.html">Meu primeiro projeto</a>
         <a href="projeto_atual.html">Meu projeto atual</a>
         <a href="dificuldades.html">Dificuldades</a>
-        <a href="#">Evolução Pessoal</a>
+        <a href="evolucao_pessoal.html">Evolução Pessoal</a>
     </div>
 
 </body>
