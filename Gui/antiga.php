f19f351df0cd033fbe11f792593f3b4305449bc8
Guilherme Soares <guisenai.oliveira11@gmail.com>
2024-11-25 14:06:24 -0300

    terminando pagina antiga

--- Gui/antiga.php
+++ Gui/antiga.php
@@ -4,31 +4,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
     <title>Document</title>
 </head>
 
 <body>
     <header>
-        <h1>Site ANTIGO</h1>
-        <div class="proximo">
-            <a href="#">Proximo</a>
+        <div class="logo">
+            <h1>Site ANTIGO</h1>
 
         </div>
+
+        <div class="botoes">
+            <div class="proximo">
+                <a href="index.php">Voltar</a>
+            </div>
+
+
+            <div class="proximo">
+                <a href="atual.php">Proximo</a>
+            </div>
+        </div>
+
+
+
     </header>
-    <main>
-        <div class="container">
+
+
+    <main class="container">
+
+
+        <div >
             <p>Essa foi a página Inicial, a qual apresentei na primeira Feira de Tecnologia:</p>
             <div><img src="paginafrente.png" alt=""></div>
+
             <p>Coloquei em prática tudo o que aprendi, desenvolvendo uma página para apresentar a profissão
-                caminhoneiro, divindo essa em outras, separadas em profissão, marcas famosas, empresas envolvidas...  </p>
-                <div><img src="empresas.png" alt=""></div>
-                <div><img src="outraparte.png" alt=""></div>
-                <p>No fim uma pagina para contatos onde esta localizada o feedback.</p>
-                <div><img src="footer.png" alt=""></div>
-                
+                caminhoneiro, divindo essa em outras, separadas em profissão, marcas famosas, empresas envolvidas...
+            </p>
+            <div><img src="empresas.png" alt=""></div>
+
+            <div><img src="outraparte.png" alt=""></div>
+
+            <p>No fim uma pagina para contatos onde esta localizada o feedback.</p>
+            <div><img src="futer.png" alt=""></div>
+
         </div>
-        <a href="#">Proximo</a>
+
     </main>
+
+
+
+    <div class="botoes">
+        <div class="proximo">
+            <a href="index.php">Voltar</a>
+        </div>
+
+
+        <div class="proximo">
+            <a href="atual.php">Proximo</a>
+        </div>
+    </div>
 
 
 
--- Gui/index.php
+++ Gui/index.php
@@ -10,7 +10,7 @@
 
 <body>
     <header>
-        <div class="logo"><img src="eufotobola.jpg" alt=""><h1>Evolução SENAI</h1></div>
+        <div class="logo"><img src="eufotobola.jpg" alt=""><h1>GUI</h1></div>
         
 
     </header>
@@ -18,17 +18,20 @@
 
     <main>
         <div class="menu">
-            <div class="link"><a href="index.php">Inicio</a></div>
+            <h1>Evolução SENAI</h1>
+
+
             <div class="link"><a href="antiga.php">Página Antiga</a></div>
             <div class="link"><a href="atual.php">Página Atual</a></div>
             <div class="link"><a href="conclusao.php">Conclusão</a></div>
         </div>
     </main>
     <footer>
-        <div>
-            <p>Guilherme Soares de Oliveira</p>
-            <p> © Todos os direitos reservados</p>
-        </div>
+        
+
+            <div><p>Guilherme Soares de Oliveira</p></div>
+            <div><p> © Todos os direitos reservados</p></div>
+        
 
     </footer>
 </body>
