f9cb7d6eb7a6e18b5cf615f3323e278245a0d39e
Roberto H Silva <robertosilva.sesisenai@gmail.comgit config --global user.namegit config --global user.email robertosilva.sesisenai@gmail.com>
2024-11-28 08:07:22 -0300

    pequenas alterações no botao de voltar

--- angelo/index.php
+++ angelo/index.php
@@ -1,48 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estileira.css">
+    <style>
+        .header-button {
+            display: flex;
+            gap: 20px;
+        }
+    </style>
     <title>Document</title>
 </head>
-<body>
-<header class="header">
-    <div class="header-container">
-        <img src="imagens/Untitled Project.jpg" alt="eu joia imagem nao aparece" class="header-logo">
-        <div class="header-text">
-            <h1 class="header-title">EVOLUÇÃO AO LONGO DO CURSO</h1>
-            <h2 class="header-subtitle"> ANGELO OSTROSKI</h2>
-        </div>
-        
-    </div>
-    <a href="../angelo/KOBE_MAMBA/index.html" class="header-button">clique aqui para ver meu ultimo projeto</a>
-    </header>
-    
-    <div class="image-section">
-    <div class="image-container">
-        <img src="imagens/futebol site meu.png" alt="Imagem 1" class="section-image">
-        <ul class="image-text">
-            <li>Não estruturar o HTML corretamente.</li>
-            <li>Esquecer de fechar tags ou usar tags incorretas.</li>
-            <li>Não focar em fazer um site responsivo.</li>
-            <li>Escolher cores com baixo contraste ou fontes pequenas.</li>
-            <li>Não ajustar as imagens para um tamanho padrão.</li>
-        </ul>
-    </div>
-    <div class="image-container">
-        <img src="imagens/KOBE BRYANT.png" alt="Imagem 2" class="section-image">
-        <ul class="image-text">
-            <li>consigo entender a logica</li>
-            <li>lembro de fechar as tags na maioria das vezes </li>
-            <li>consigo fazer o site responsivo </li>
-            <li>melhorei um pouco nas cores </li>
-            <li>todas imagens segem um padrão</li>
-        </ul>
-    </div>
-</div>
 
-   
+<body>
+    <header class="header">
+        <div class="header-container">
+            <img src="imagens/Untitled Project.jpg" alt="eu joia imagem nao aparece" class="header-logo">
+            <div class="header-text">
+                <h1 class="header-title">EVOLUÇÃO AO LONGO DO CURSO</h1>
+                <h2 class="header-subtitle"> ANGELO OSTROSKI</h2>
+            </div>
+
+        </div>
+        <a href="../angelo/KOBE_MAMBA/index.html" class="header-button">clique aqui para ver meu ultimo projeto</a>
+
+    </header>
+
+    <div class="image-section">
+        <div class="image-container">
+            <img src="imagens/futebol site meu.png" alt="Imagem 1" class="section-image">
+            <ul class="image-text">
+                <li>Não estruturar o HTML corretamente.</li>
+                <li>Esquecer de fechar tags ou usar tags incorretas.</li>
+                <li>Não focar em fazer um site responsivo.</li>
+                <li>Escolher cores com baixo contraste ou fontes pequenas.</li>
+                <li>Não ajustar as imagens para um tamanho padrão.</li>
+            </ul>
+        </div>
+        <div class="image-container">
+            <img src="imagens/KOBE BRYANT.png" alt="Imagem 2" class="section-image">
+            <ul class="image-text">
+                <li>consigo entender a logica</li>
+                <li>lembro de fechar as tags na maioria das vezes </li>
+                <li>consigo fazer o site responsivo </li>
+                <li>melhorei um pouco nas cores </li>
+                <li>todas imagens segem um padrão</li>
+            </ul>
+        </div>
+    </div>
+    <a href="../Evolucao.php" class="header-button">Projetos do grupo</a>
+
+
 
 </body>
+
 </html>
